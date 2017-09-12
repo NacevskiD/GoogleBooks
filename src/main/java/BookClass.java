22d62a3e41ba5facd19b6995cb752a1353854865
TestName <me@email.com>
2017-09-12 01:24:09 -0500

    fixed some version errors

--- src/main/java/BookClass.java
+++ src/main/java/BookClass.java
@@ -10,14 +10,20 @@ public class BookClass {
     String description;
     String isbn;
     Double rating;
-    BookClass(String title, String author,String publisher,String description,String isbn,Double rating){
+    String buyLink;
+    BookClass(String title, String author,String publisher,String description,String isbn,Double rating,String buyLink){
         this.title = title;
         this.author = author;
         this.publisher = publisher;
         this.description = description;
         this.isbn = isbn;
         this.rating = rating;
+        this.buyLink = buyLink;
 
+    }
+
+    String returnTitle(){
+        return this.title;
     }
     public String toString(){
         return title + author + publisher + description + isbn + Double.toString(rating);
--- src/main/java/BookGUI.java
+++ src/main/java/BookGUI.java
@@ -2,14 +2,20 @@ import com.sun.xml.internal.bind.v2.TODO;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONString;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
 
 /**
  * Created by David on 9/10/2017.
@@ -20,70 +26,23 @@ public class BookGUI extends JFrame{
     private JComboBox searchBy;
     private JTextField searchText;
     private JButton searchButton;
+    private JScrollPane googleBooksList;
     private JButton addButton;
     private JButton removeButton;
-    private JScrollPane googleBooksList;
     private JLabel statusLabel;
-    private JList<BookClass> googleBooks;
-    private DefaultListModel<BookClass> googleBooksModel;
+    private JList<String> googleBooks;
+    private JTextArea bookNameTextArea;
+    private JTextField authorTextArea;
+    private JTextField publisherTextField;
+    private JTextField isbnTextField;
+    private JTextField ratingTextField;
+    private JTextArea descriptionTextArea;
+    private JTextArea buyLinkTextArea;
+    private JTextField buyLinkTextField;
+    private JTextField descriptionTextField;
+    private DefaultListModel<String> googleBooksModel;
+    private int searchType;
     ArrayList<BookClass> allBooks = new ArrayList<BookClass>();
-
-    private void findBook(String query) {
-        String key = getKey();
-        String baseURL = "https://www.googleapis.com/books/v1/volumes?q=%s&key=%s";
-        String url = String.format(baseURL, query, key);
-        InputStream stream = null;
-        try {
-            stream = new URL(url).openConnection().getInputStream();
-
-        }
-        catch (IOException ioe){
-            statusLabel.setText("IO exception");
-        }
-        InputStreamReader reader = new InputStreamReader(stream);
-        BufferedReader bufferedReader = new BufferedReader(reader);
-
-        StringBuilder builder = new StringBuilder();
-
-        String line;
-        try {
-            while ((line = bufferedReader.readLine()) != null) {
-                builder.append(line);
-            }
-        }catch (IOException ioe){
-            statusLabel.setText("Problem");
-        }
-        String responseString = builder.toString();
-
-
-        JSONObject jsonObject = new JSONObject(responseString);
-        JSONArray items = jsonObject.getJSONArray("items");
-
-        for (int count = 0; count != items.length();count++) {
-            JSONObject volumeInfo = items.getJSONObject(count).getJSONObject("volumeInfo");
-            String title = volumeInfo.getString("title");
-            String description = volumeInfo.getString("description");
-            String publisher = volumeInfo.getString("publisher");
-            String authors;
-            String isbn;
-            try {
-                JSONArray authorList = volumeInfo.getJSONArray("authors");
-            }catch (JSONException je) {
-                System.out.println("No author(s) listed for this book.");
-            }
-
-            double googleRating = 0;
-            try{
-                googleRating = volumeInfo.getDouble("averageRating");
-            }catch (JSONException je) {
-                System.out.println("No rating provided for this book.");
-            }
-            BookClass book = new BookClass(title,authors="none",publisher,description,isbn ="none",googleRating);
-            allBooks.add(book);
-
-
-        }}
-
 
 
 
@@ -91,23 +50,37 @@ public class BookGUI extends JFrame{
     BookGUI(){
         populateSearchType();
 
-        googleBooks = new JList<BookClass>();
-        googleBooksList = new JScrollPane(googleBooks);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        googleBooksModel = new DefaultListModel<String>();
+        googleBooks.setModel(googleBooksModel);
         setContentPane(mainPanel);
         pack();
-
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        googleBooksModel = new DefaultListModel<BookClass>();
-        googleBooks.setModel(googleBooksModel);
         setVisible(true);
+
 
 
         searchButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                allBooks.clear();
+                googleBooksModel.clear();
                 String query = searchText.getText();
                 String formatedQuery = formatQuery(query);
                 findBook(formatedQuery);
                 setListData(allBooks);
+            }
+        });
+
+        googleBooks.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                int selected = googleBooks.getSelectedIndex();
+                displaySelectedBook(selected);
+            }
+        });
+        searchBy.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                searchType = searchBy.getSelectedIndex();
             }
         });
 /*       addButton.addActionListener(new ActionListener() {
@@ -123,9 +96,9 @@ public class BookGUI extends JFrame{
     }
     private void populateSearchType(){
         ArrayList<String> searchList = new ArrayList<String>();
-        searchList.add("Author");
+        searchList.add("Title");
         searchList.add("ISBN");
-        searchList.add("Name");
+        searchList.add("Author");
 
         for (String item : searchList){
             searchBy.addItem(item);
@@ -163,9 +136,183 @@ public class BookGUI extends JFrame{
 
     void setListData(ArrayList<BookClass> data){
         googleBooksModel.clear();
-        for (BookClass book: data){
-            googleBooksModel.addElement(book);
+        String[] names = new String[data.size()];
+        int count = 0;
+        for (BookClass book:data){
+            names[count] = book.title;
+            googleBooksModel.add(0,book.title);
         }
+
+        Collections.reverse(allBooks);
+
+
+    }
+    private void findBook(String query) {
+        String key = getKey();
+        String baseURL = "https://www.googleapis.com/books/v1/volumes?q=%s&key=%s";
+        String url = String.format(baseURL, query, key);
+        InputStream stream = null;
+        try {
+            stream = new URL(url).openConnection().getInputStream();
+
+        }
+        catch (IOException ioe){
+            statusLabel.setText("IO exception");
+        }
+        InputStreamReader reader = new InputStreamReader(stream);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+
+        StringBuilder builder = new StringBuilder();
+
+        String line;
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                builder.append(line);
+            }
+        }catch (IOException ioe){
+            statusLabel.setText("Problem");
+        }
+        String responseString = builder.toString();
+
+
+        JSONObject jsonObject = new JSONObject(responseString);
+        JSONArray items = jsonObject.getJSONArray("items");
+
+        for (int count = 0; count != items.length();count++) {
+            JSONObject volumeInfo = items.getJSONObject(count).getJSONObject("volumeInfo");
+            String id = items.getJSONObject(count).getString("id");
+            String title = volumeInfo.getString("title");
+            String description = volumeInfo.getString("description");
+            String publisher = "not found";
+            try{
+            publisher = volumeInfo.getString("publisher");
+            }catch (JSONException jse){
+                System.out.println("Publisher not found");
+            }
+            String authors = "";
+            String isbn;
+            JSONArray authorList = new JSONArray();
+            try {
+                authorList = volumeInfo.getJSONArray("authors");
+            }catch (JSONException je) {
+                System.out.println("No author(s) listed for this book.");
+            }
+
+            for (Object author:authorList){
+                author = author.toString();
+                authors += author+", ";
+            }
+            authors = authors.substring(0, authors.length() - 2);
+
+
+            double googleRating = 0;
+            try{
+                googleRating = volumeInfo.getDouble("averageRating");
+            }catch (JSONException je) {
+                System.out.println("No rating provided for this book.");
+            }
+            isbn = getIsbn(id);
+            String buyLink = getBuyLink(id);
+
+            BookClass book = new BookClass(title,authors,publisher,description,isbn,googleRating,buyLink);
+            allBooks.add(book);
+
+
+        }}
+
+    private String getIsbn(String id){
+        String key = getKey();
+        String baseURL = "https://www.googleapis.com/books/v1/volumes/%s?key=%s";
+        String url = String.format(baseURL, id, key);
+        InputStream stream = null;
+        try {
+            stream = new URL(url).openConnection().getInputStream();
+
+        }
+        catch (IOException ioe){
+            statusLabel.setText("IO exception");
+        }
+        InputStreamReader reader = new InputStreamReader(stream);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+
+        StringBuilder builder = new StringBuilder();
+
+        String line;
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                builder.append(line);
+            }
+        }catch (IOException ioe){
+            statusLabel.setText("Problem");
+        }
+        String responseString = builder.toString();
+
+
+        JSONObject jsonObject = new JSONObject(responseString);
+        JSONArray isbnArray = new JSONArray();
+        String isbn = "";
+        try{
+            isbnArray = jsonObject.getJSONObject("volumeInfo").getJSONArray("industryIdentifiers");
+            isbn = isbnArray.getJSONObject(0).getString("identifier");
+
+        }catch (JSONException jse){
+            isbn = "NA";
+        }
+
+        return isbn;
     }
 
+    private String getBuyLink(String id){
+        String key = getKey();
+        String baseURL = "https://www.googleapis.com/books/v1/volumes/%s?key=%s";
+        String url = String.format(baseURL, id, key);
+        InputStream stream = null;
+        try {
+            stream = new URL(url).openConnection().getInputStream();
+
+        }
+        catch (IOException ioe){
+            statusLabel.setText("IO exception");
+        }
+        InputStreamReader reader = new InputStreamReader(stream);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+
+        StringBuilder builder = new StringBuilder();
+
+        String line;
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                builder.append(line);
+            }
+        }catch (IOException ioe){
+            statusLabel.setText("Problem");
+        }
+        String responseString = builder.toString();
+
+
+        JSONObject jsonObject = new JSONObject(responseString);
+
+
+        JSONObject saleInfo = jsonObject.getJSONObject("saleInfo");
+        String buyLink = "";
+        try{
+            buyLink = saleInfo.getString("buyLink");
+        }catch (JSONException jse){
+            buyLink = "NA";
+        }
+
+        return buyLink;
+
+    }
+
+
+    private void displaySelectedBook(int selected){
+        bookNameTextArea.setText(allBooks.get(selected).title);
+        authorTextArea.setText(allBooks.get(selected).author);
+        publisherTextField.setText(allBooks.get(selected).publisher);
+        isbnTextField.setText(allBooks.get(selected).isbn);
+        ratingTextField.setText(Double.toString(allBooks.get(selected).rating));
+        descriptionTextArea.setText(allBooks.get(selected).description);
+        buyLinkTextArea.setText(allBooks.get(selected).buyLink);
+    }
 }
